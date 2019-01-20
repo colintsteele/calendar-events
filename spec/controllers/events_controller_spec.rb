6a79b490a601a798befd075d5b667dc3906c7a30
Colin Steele <colin.steele20@gmail.com>
2019-01-20 15:31:02 +0000

    Added a controller spec to make sure Events are saved corectly

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe EventsController, type: :controller do
+  let(:sample) { File.read(Rails.root.join('test/fixtures/files/sample.json')) }
+
   context 'GET index' do
     it 'renders nothing when there are no events' do
       get :index
       expect(response).to render_template("index")
+    end
+
+  end
+  context 'POST events' do
+    it 'saves a parsable json to params' do
+      post :create, params: JSON.parse(sample)
+      expect(Event.last.parsed_payload).to be_a(Hash)
     end
   end
 end
